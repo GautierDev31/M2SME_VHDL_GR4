7f597f7b3c8c9e02845030e21f53354d36998d19
Alexandre REGNERE <a.regnere@outlok.fr>
2020-10-15 19:13:56 +0200

    SOPC + chenillard

---
+++ SOPC/software/sopc_test/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+  ../sopc_test_bsp//HAL/inc/sys/alt_stdio.h ../sopc_test_bsp/system.h \
+  ../sopc_test_bsp/linker.h
+
+../sopc_test_bsp//HAL/inc/sys/alt_stdio.h:
+
+../sopc_test_bsp/system.h:
+
+../sopc_test_bsp/linker.h:
